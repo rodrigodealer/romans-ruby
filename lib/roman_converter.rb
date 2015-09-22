230cd6c8ce9d8d0921cd08546bf935b8b3f5d618
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-09-22 09:16:30 -0300

    Extracting roman number validation to a private method

--- lib/roman_converter.rb
+++ lib/roman_converter.rb
@@ -1,6 +1,13 @@
 # Roman numbers converter class
 class RomanConverter
   def self.to_integer(roman_number, result = 0)
-    return result if roman_number.nil? || roman_number.empty?
+    return result unless is_roman?(roman_number)
   end
+
+  private
+
+  def self.is_roman?(roman_number)
+    !roman_number.nil? && !roman_number.empty?
+  end
+
 end
