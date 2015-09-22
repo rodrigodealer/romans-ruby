ee1a9dad515c64bbe6237d0a7a550b09144b524b
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-09-22 08:14:45 -0300

    Adding initial specs for roman number to integer conversion

---
+++ lib/roman_converter.rb
@@ -0,0 +1,2 @@
+class RomanConverter
+end
---
+++ spec/roman_converter_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'roman_converter'
+
+RSpec.describe RomanConverter do
+  describe 'simple conversion' do
+    xit 'returns a conversion of a simple roman number' do
+    end
+
+    xit 'returns a conversion for a two chars roman number but returns 9' do
+    end
+  end
+
+  describe 'complex conversion' do
+    xit 'returns a conversion for a three chars roman number' do
+    end
+
+    xit 'returns a conversion for a four chars roman number' do
+    end
+
+    xit 'returns a conversion for the biggest roman number' do
+    end
+  end
+end
